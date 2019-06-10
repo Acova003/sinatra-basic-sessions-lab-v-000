541d6a9dc51266d955153587d6d99588a735e466
Amee Covarrubias <ameecova@yahoo.com>
2019-06-10 23:51:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end 
+  
   configure do
     enable :sessions
     set :session_secret, "vixenisthebest"
